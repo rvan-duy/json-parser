fec9af6ce431a21449535c5d0d8e9cea6bca6d27
Ruben <rvan-duy@ruben-laptop.change-is.com>
2025-03-12 22:29:49 +0100

    add failing test

--- test/test_json-data.rb
+++ test/test_json-data.rb
@@ -10,4 +10,8 @@ class TestJsonData < Minitest::Test
     def test_test
         assert_output("test\n") { @data.test }
     end
+
+    def test_fail
+        assert_output("fail\n") { @data.test }
+    end
 end
